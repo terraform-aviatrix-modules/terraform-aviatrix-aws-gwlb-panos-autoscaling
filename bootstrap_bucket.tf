ea2f88593e618ca9087f4e45bd80dbfeefe5dda8
Dennizz <root@ipaddr.nl>
2021-06-05 11:54:07 +0200

    fix bootstrap

--- bootstrap_bucket.tf
+++ bootstrap_bucket.tf
@@ -26,28 +26,28 @@ resource "aws_s3_bucket_object" "folder_software" {
   key    = "software/"
   source = "/dev/null"
 }
-resource "aws_s3_bucket_object" "xml" {
-  bucket = aws_s3_bucket.aviatrix-palo-alto-bootstrap-bucket.id
-  key    = "config/bootstrap.xml"
-  source = "bootstrap.xml"
-}
+
 resource "aws_s3_bucket_object" "init" {
   bucket = aws_s3_bucket.aviatrix-palo-alto-bootstrap-bucket.id
   key    = "config/init-cfg.txt"
-  source = "init-cfg.txt"
+  source = "${path.module}/resources/init-cfg.txt"
 }
+
 resource "aws_iam_role" "FirewallBootstrapRole" {
   name               = "FirewallBootstrapRole"
-  assume_role_policy = file("${path.module}/resources/bootstrap_iam_policy.json")
+  assume_role_policy = file("${path.module}/resources/bootstrap_iam_role.json")
 }
+
 resource "aws_iam_policy" "aviatrix-palo-alto-bootstrap-iam-policy" {
   name   = "aviatrix-palo-alto-bootstrap-iam-policy"
   policy = file("${path.module}/resources/bootstrap_iam_policy.json")
 }
+
 resource "aws_iam_role_policy_attachment" "aviatrix-palo-alto-bootstrap-iam-policy-role-attachment" {
   role       = aws_iam_role.FirewallBootstrapRole.name
   policy_arn = aws_iam_policy.aviatrix-palo-alto-bootstrap-iam-policy.arn
 }
+
 resource "aws_iam_instance_profile" "aviatrix-palo-alto-bootstrap-iam-instance-profile" {
   name = "aviatrix-palo-alto-bootstrap-iam-instance-profile"
   role = aws_iam_role.FirewallBootstrapRole.name
