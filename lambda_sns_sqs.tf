6e03ec83aa7ce39b571bd4882b3d5aea6297e483
Dennizz <root@ipaddr.nl>
2021-06-05 10:19:18 +0200

    add more stuff

--- lambda_sns_sqs.tf
+++ lambda_sns_sqs.tf
@@ -13,6 +13,21 @@ resource "aws_iam_role_policy_attachment" "LambdaExecutionPolicyAttachment" {
   policy_arn = aws_iam_policy.LambdaExecutionPolicy.arn
 }
 
+resource "aws_iam_role" "ASGNotifierRole" {
+  name               = "ASGNotifierRole"
+  assume_role_policy = file("${path.module}/resources/lambda_iam_role.json")
+}
+
+resource "aws_iam_policy" "ASGNotifierRolePolicy" {
+  name   = "ASGNotifierRolePolicy"
+  policy = file("${path.module}/resources/lambda_iam_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "ASGNotifierRolePolicyAttachment" {
+  role       = aws_iam_role.ASGNotifierRole.name
+  policy_arn = aws_iam_policy.ASGNotifierRolePolicy.arn
+}
+
 resource "aws_security_group" "VPCSecurityGroup" {
   name        = "VPCSecurityGroup"
   description = "Allow all"
@@ -51,15 +66,25 @@ resource "aws_lambda_function" "FwInit" {
   timeout     = 900
 
   vpc_config {
-    # Every subnet should be able to reach an EFS mount target in the same Availability Zone. Cross-AZ mounts are not permitted.
     subnet_ids         = [aws_subnet.LambdaMGMTSubnetAz1.id, aws_subnet.LambdaMGMTSubnetAz2.id, ]
     security_group_ids = [aws_security_group.VPCSecurityGroup.id]
   }
+}
 
-  environment {
-    variables = {
-      foo = "bar"
-    }
+resource "aws_lambda_function" "InitLambda" {
+  filename         = "${path.module}/resources/panw-aws.zip"
+  function_name    = "InitLambda"
+  role             = aws_iam_role.LambdaExecutionRole.arn
+  handler          = "init.lambda_handler"
+  source_code_hash = filebase64sha256("${path.module}/resources/panw-aws.zip")
+
+  runtime     = "python3.6"
+  memory_size = 256
+  timeout     = 900
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.LambdaMGMTSubnetAz1.id, aws_subnet.LambdaMGMTSubnetAz2.id, ]
+    security_group_ids = [aws_security_group.VPCSecurityGroup.id]
   }
 }
 
@@ -75,4 +100,13 @@ resource "aws_sns_topic_subscription" "LambdaENISNSTopic" {
   topic_arn = aws_sns_topic.LambdaENISNSTopic.arn
   protocol  = "lambda"
   endpoint  = aws_lambda_function.FwInit.arn
+}
+
+resource "aws_lambda_permission" "LambdaENIPermission" {
+  statement_id  = "AllowSNSFromLambda"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.FwInit.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.LambdaENISNSTopic.arn
+  qualifier     = aws_lambda_alias.test_alias.name
 }
